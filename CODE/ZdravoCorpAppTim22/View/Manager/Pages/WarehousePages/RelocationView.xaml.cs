916b210f79e2918f379071d955a62c2b9307710e
Rade Stojanovic <stojanovicrade614@gmail.com>
2022-04-22 20:46:08 +0200

    Room and time selection

--- CODE/ZdravoCorpAppTim22/View/Manager/Pages/WarehousePages/RelocationView.xaml.cs
+++ CODE/ZdravoCorpAppTim22/View/Manager/Pages/WarehousePages/RelocationView.xaml.cs
@@ -1,27 +1,94 @@
 ﻿using Model;
+using System;
 using System.Diagnostics;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Navigation;
+using ZdravoCorpAppTim22.Model.Utility;
+using ZdravoCorpAppTim22.View.Manager.ViewModels;
+using ZdravoCorpAppTim22.View.Manager.Views.RoomAppointments;
 
 namespace ZdravoCorpAppTim22.View.Manager.Pages.WarehousePages
 {
     public partial class RelocationView : Page
     {
         readonly WarehouseView ParentPage;
+        public RoomViewModel RoomViewModel;
+        public Interval Interval;
+        public Room DestinationRoom;
         public RelocationView(WarehouseView parent)
         {
             InitializeComponent();
+            RoomViewModel = new RoomViewModel();
+            DataContext = RoomViewModel;
             ParentPage = parent;
-            foreach (Equipment eq in parent.SelectedEquipment)
+            StartTimeGroup.Visibility = Visibility.Hidden;
+            EndTimeGroup.Visibility = Visibility.Hidden;
+        }
+
+        public void StartDateSelected(object sender, EventArgs e)
+        {
+            Interval.Start = ((CustomDatePicker)sender).SelectedDate;
+            if (DateTime.Compare(Interval.Start, new DateTime()) > 0)
             {
-                Debug.WriteLine(eq.Name);
+                SelectStartTimeContent.Content = Interval.Start.ToString();
+                EndTimeGroup.Visibility = Visibility.Visible;
+                //RoomEdit.Visibility = Visibility.Hidden;
+                Interval.End = new DateTime();
+                SelectEndTimeContent.Content = "";
+            }
+            else
+            {
+                EndTimeGroup.Visibility = Visibility.Hidden;
+            }
+        }
+
+        public void EndDateSelected(object sender, EventArgs e)
+        {
+            Interval.End = ((CustomDatePicker)sender).SelectedDate;
+            if (DateTime.Compare(Interval.End, new DateTime()) > 0)
+            {
+                SelectEndTimeContent.Content = Interval.End.ToString();
+                //RoomEdit.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                //RoomEdit.Visibility = Visibility.Hidden;
             }
         }
 
         private void BackButton_Click(object sender, RoutedEventArgs e)
         {
             NavigationService.Navigate(ParentPage);
+        }
+
+        private void DataGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Room room = (Room)DataGrid.SelectedItem;
+            if(room != null)
+            {
+                DestinationRoom = room;
+                StartTimeGroup.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                StartTimeGroup.Visibility = Visibility.Hidden;
+                EndTimeGroup.Visibility = Visibility.Hidden;
+            }
+        }
+
+        private void ButtonSelectStartTime_Click(object sender, RoutedEventArgs e)
+        {
+            var RelocationStartDateView = new SelectTimePage(this, DestinationRoom);
+            RelocationStartDateView.CustomDatePicker.DateSelectedEvent += StartDateSelected;
+            this.NavigationService.Navigate(RelocationStartDateView);
+        }
+
+        private void ButtonSelectEndTime_Click(object sender, RoutedEventArgs e)
+        {
+            var RelocationEndDateView = new SelectTimePage(this, DestinationRoom, Interval.Start);
+            RelocationEndDateView.CustomDatePicker.DateSelectedEvent += EndDateSelected;
+            this.NavigationService.Navigate(RelocationEndDateView);
         }
     }
 }
