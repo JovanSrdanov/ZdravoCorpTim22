a9bb504cac277487d97262c3e1dcab2ca5a5d691
JovanSrdanov <93533441+JovanSrdanov@users.noreply.github.com>
2022-04-30 17:28:41 +0200

    FixovanjeBrisanjaAppointmenta

--- CODE/ZdravoCorpAppTim22/Repository/MedicalAppointmentRepository.cs
+++ CODE/ZdravoCorpAppTim22/Repository/MedicalAppointmentRepository.cs
@@ -20,5 +20,23 @@ namespace Repository
                 return instance;
             }
         }
+
+        public override void DeleteByID(int id)
+        {
+
+            MedicalAppointment medicalAppointment = Instance.GetByID(id);
+
+            Patient patient = PatientRepository.Instance.GetByID(medicalAppointment.Patient.Id);
+            patient.MedicalAppointment.Remove(medicalAppointment);
+
+            
+            Doctor doctor = DoctorRepository.Instance.GetByID(medicalAppointment.Doctor.Id);
+            doctor.MedicalAppointment.Remove(medicalAppointment);
+
+            Room room = RoomRepository.Instance.GetByID(medicalAppointment.Room.Id);
+            room.MedicalAppointment.Remove(medicalAppointment);
+
+            base.DeleteByID(id);
+        }
     }
 }
