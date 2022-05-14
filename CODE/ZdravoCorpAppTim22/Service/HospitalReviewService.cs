b192d5f974eeef29e1521f4b297c36dd56f778fc
JovanSrdanov <93533441+JovanSrdanov@users.noreply.github.com>
2022-05-14 19:51:31 +0200

    Napravljen HospitalReview ali bez WPF

---
+++ CODE/ZdravoCorpAppTim22/Service/HospitalReviewService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+using ZdravoCorpAppTim22.Model;
+using ZdravoCorpAppTim22.Service.Generic;
+
+namespace ZdravoCorpAppTim22.Service
+{
+    public class HospitalReviewService : GenericService<HospitalReviewRepository, HospitalReview>
+    {
+        private static HospitalReviewService instance;
+
+        private HospitalReviewService() : base(HospitalReviewRepository.Instance) { }
+
+        public static HospitalReviewService Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new HospitalReviewService();
+                }
+
+                return instance;
+            }
+        }
+    }
+}
