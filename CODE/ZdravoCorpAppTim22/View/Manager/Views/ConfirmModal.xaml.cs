06ab6cd096a2783da7cac9a84b7c96c4fbda0428
Rade Stojanovic <stojanovicrade614@gmail.com>
2022-05-16 14:32:42 +0200

    Confirm Modal finished

---
+++ CODE/ZdravoCorpAppTim22/View/Manager/Views/ConfirmModal.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Windows;
+
+namespace ZdravoCorpAppTim22.View.Manager.Views
+{
+
+    public partial class ConfirmModal : Window
+    {
+        public string InfoText { get; private set; }
+        public bool IsConfirmed { get; private set; }
+        private ConfirmModal(string infoText)
+        {
+            InitializeComponent();
+            DataContext = this;
+            InfoText = infoText;
+            IsConfirmed = false;
+        }
+
+        public static bool Show(string message)
+        {
+            ConfirmModal confirmModal = new ConfirmModal(message)
+            {
+                Owner = ManagerHome.Instance
+            };
+            confirmModal.ShowDialog();
+
+            return confirmModal.IsConfirmed;
+        }
+
+        private void ConfirmEvent(object sender, RoutedEventArgs e)
+        {
+            IsConfirmed = true;
+            this.Close();
+        }
+
+        private void CancelEvent(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
