aea763ea5385beb6fb3f5f0c677be9209b58ae82
Rade Stojanovic <stojanovicrade614@gmail.com>
2022-06-02 19:14:32 +0200

    ratings UI updates

---
+++ CODE/ZdravoCorpAppTim22/View/Manager/Pages/StaffPages/StaffRatingsView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Model;
+using System.Windows.Controls;
+using ZdravoCorpAppTim22.View.Manager.ViewModels.StaffViewModels;
+
+namespace ZdravoCorpAppTim22.View.Manager.Pages.StaffPages
+{
+    public partial class StaffRatingsView : Page
+    {
+        readonly StaffRatingsViewModel ViewModel;
+        public StaffRatingsView(Doctor doctor)
+        {
+            InitializeComponent();
+            ViewModel = new StaffRatingsViewModel(doctor);
+            DataContext = ViewModel;
+        }
+    }
+}
