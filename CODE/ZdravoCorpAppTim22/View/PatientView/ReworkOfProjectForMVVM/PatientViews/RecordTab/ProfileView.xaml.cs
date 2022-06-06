a454cb3a7c2a1a951d81e7a9285945bde7a8af3b
JovanSrdanov <93533441+JovanSrdanov@users.noreply.github.com>
2022-06-06 07:16:47 +0200

    Pocetni prikaz profila

---
+++ CODE/ZdravoCorpAppTim22/View/PatientView/ReworkOfProjectForMVVM/PatientViews/RecordTab/ProfileView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Model;
+using System.Windows.Controls;
+using ZdravoCorpAppTim22.Controller;
+
+namespace ZdravoCorpAppTim22.View.PatientView.ReworkOfProjectForMVVM.PatientViews.RecordTab
+{
+
+    public partial class ProfileView : Page
+    {
+        public ProfileView()
+        {
+            InitializeComponent();
+            ProfileViewModel profileViewModel = new ProfileViewModel((Patient)AuthenticationController.Instance.GetLoggedUser());
+            this.DataContext = profileViewModel;
+        }
+    }
+}
---
+++ CODE/ZdravoCorpAppTim22/View/PatientView/ReworkOfProjectForMVVM/PatientViews/RecordTab/ProfileViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Model;
+
+namespace ZdravoCorpAppTim22.View.PatientView.ReworkOfProjectForMVVM.PatientViews.RecordTab
+{
+    public class ProfileViewModel:ViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+        public string Jmbg { get; set; }
+        public DateTime Birthday { get; set; }
+        public string Phone { get; set; }
+        public Gender Gender { get; set; }
+        public string Adress { get; set; }
+
+
+
+        public ProfileViewModel(Patient patient)
+        {
+            Id = patient.Id;
+            Name = patient.Name;
+            Surname = patient.Surname;
+            Email = patient.Email;
+            Jmbg = patient.Jmbg;
+            Birthday = patient.Birthday;
+            Phone = patient.Phone;
+            Gender = patient.Gender;
+            Adress = patient.address.ToString();
+
+        }
+    }
+}
