35158604543dce85499e3569ea9ed7b012de6e7d
JovanSrdanov <93533441+JovanSrdanov@users.noreply.github.com>
2022-06-12 22:08:46 +0200

    JovanHCI GOTOV part 1

--- CODE/ZdravoCorpAppTim22/View/PatientView/ReworkOfProjectForMVVM/PatientViews/TherapyTab/TherapyTableView.xaml.cs
+++ CODE/ZdravoCorpAppTim22/View/PatientView/ReworkOfProjectForMVVM/PatientViews/TherapyTab/TherapyTableView.xaml.cs
@@ -1,10 +1,11 @@
-﻿using System;
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.IO;
 using System.Windows.Controls;
 using System.Windows.Forms;
-using Controller;
-using Model;
 using ZdravoCorpAppTim22.Controller;
 using ZdravoCorpAppTim22.Model;
 
@@ -50,10 +51,99 @@ namespace ZdravoCorpAppTim22.View.PatientView.ReworkOfProjectForMVVM.PatientView
         private void Button_Click(object sender, System.Windows.RoutedEventArgs e)
         {
             Patient patient = (Patient)AuthenticationController.Instance.GetLoggedUser();
-            PersonalNote personal = new PersonalNote(2323, patient, 3,"Probaaa","ma da",DateTime.Now);
-            PersonalNoteController.Instance.Create(personal);
+            DateTime StartDate = (DateTime)StartTherapyReport.SelectedDate;
+            DateTime EndDate = StartDate.AddDays(7);
 
-            MessageBox.Show(PersonalNoteController.Instance.GetByID(personal.Id).Patient.Name);
+            using (SaveFileDialog sfd = new SaveFileDialog() { Filter = "PDF file|*.pdf", ValidateNames = true })
+            {
+
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+
+                    iTextSharp.text.Document doc = new iTextSharp.text.Document(PageSize.A4.Rotate());
+                    try
+                    {
+                        PdfWriter.GetInstance(doc, new FileStream(sfd.FileName, FileMode.Create));
+                        doc.Open();
+
+                        PdfPTable table = new PdfPTable(7);
+
+                        PdfPCell cell = new PdfPCell(new Phrase("Izvestaj"));
+
+                        cell.Colspan = 7;
+
+                        cell.HorizontalAlignment = 1; //0=Left, 1=Centre, 2=Right
+
+                        table.AddCell(cell);
+
+                        table.AddCell("Dan uzmianja");
+                        table.AddCell("ID");
+                        table.AddCell("Svrha terapije");
+                        table.AddCell("Dodatna upustva");
+                        table.AddCell("Lekovi");
+                        table.AddCell("Vreme uzimanja");
+                        table.AddCell("Kraj terapije");
+
+                        var medicalReceipts = patient.medicalRecord.medicalReceipt;
+
+                        for (; StartDate.Date <= EndDate.Date; StartDate = StartDate.Date.AddDays(1))
+                        {
+                            foreach (MedicalReceipt medicalReceipt in medicalReceipts)
+                            {
+                                if (medicalReceipt.EndDate.Date >= StartDate.Date)
+                                {
+                                    table.AddCell(StartDate.ToString("dd-MM-yyyy"));
+                                    table.AddCell(medicalReceipt.Id.ToString());
+                                    table.AddCell(medicalReceipt.TherapyPurpose.ToString());
+                                    table.AddCell(medicalReceipt.AdditionalInstructions.ToString());
+                                    string medications = "";
+                                    if(medicalReceipt.medicine!=null)
+                                    foreach (var med in medicalReceipt.medicine)
+                                    {
+                                        medications = medications + med.MedicineData.Name + "\n";
+
+                                    }
+
+
+                                    table.AddCell(medications);
+                                    table.AddCell(medicalReceipt.Time);
+                                    table.AddCell(medicalReceipt.EndDate.ToString("dd-MM-yyyy"));
+
+                                }
+                            }
+
+                        }
+
+                        doc.Add(table);
+
+
+
+                    }
+                    catch
+                    {
+
+                    }
+                    finally
+                    {
+                       
+                        doc.Close();
+
+                        ViewPdf viewPdf = new ViewPdf(sfd.FileName);
+
+                        viewPdf.ShowDialog();
+                       
+                      
+                    }
+
+
+                }
+
+
+            }
+            {
+               
+              
+            }
 
 
         }
---
+++ CODE/ZdravoCorpAppTim22/View/PatientView/ReworkOfProjectForMVVM/ViewPdf.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ZdravoCorpAppTim22.View.PatientView.ReworkOfProjectForMVVM
+{
+    /// <summary>
+    /// Interaction logic for ViewPdf.xaml
+    /// </summary>
+    public partial class ViewPdf : Window
+    {
+        public ViewPdf(string fileName)
+        {
+            InitializeComponent();
+            pdfWebViewer.Navigate(fileName);
+        }
+    }
+}
